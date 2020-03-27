ef890c411a46d1117b140737dd1230029c6f98b7
Apkolla.Swamy <Apkolla.Swamy@ASWAMY03>
2020-03-27 22:40:01 +0530

    updated the txt for 'FRENCH'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class AppComponent {
     this.store.dispatch({ type: 'SPANISH' });
   }
   changetoFrench() {
-    this.store.dispatch({ type: '' });
+    this.store.dispatch({ type: 'FRENCH' });
   }
 
 
