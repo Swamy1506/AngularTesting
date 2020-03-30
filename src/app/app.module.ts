e7ec6f49bb0c87689eedf894e243f33d2a0ab83d
Apkolla.Swamy <Apkolla.Swamy@ASWAMY03>
2020-03-30 21:02:21 +0530

    implemented dispatch and reducers to Add and delete operations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { simpleReducer } from './simple.reducer';
+import { ShoppingItemsComponent } from './shopping-items/shopping-items.component';
+import { ShoppingReducer } from './shopping-items/reducers/shopping.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
-    LoginComponent
+    LoginComponent,
+    ShoppingItemsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ message: simpleReducer })
+    FormsModule,
+    StoreModule.forRoot({
+      message: simpleReducer,
+      shopping: ShoppingReducer
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
